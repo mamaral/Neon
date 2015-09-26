07741c064f9aace9a5bed0bac5d9abb091b6755f
Mike Amaral <mike.amaral@ping4.com>
2015-09-25 20:54:09 -0400

    Cleaned up some documentation.

--- Source/Neon.swift
+++ Source/Neon.swift
@@ -131,19 +131,14 @@ extension UIView {
     ///
     /// Specifies a corner of a frame.
     ///
-    /// ----
     /// **TopLeft**: The upper-left corner of the frame.
     ///
-    /// ----
     /// **TopRight**: The upper-right corner of the frame.
     ///
-    /// ----
     /// **BottomLeft**: The bottom-left corner of the frame.
     ///
-    /// ----
     /// **BottomRight**: The upper-right corner of the frame.
     ///
-    /// ----
     enum Corner {
         case TopLeft
         case TopRight
@@ -157,19 +152,13 @@ extension UIView {
     ///
     /// Specifies an edge, or face, of a frame.
     ///
-    /// ----
     /// **Top**: The top edge of the frame.
     ///
-    /// ----
     /// **Left**: The left edge of the frame.
     ///
-    /// ----
     /// **Bottom**: The bottom edge of the frame.
     ///
-    /// ----
     /// **Right**: The right edge of the frame.
-    ///
-    /// ----
     ///
     enum Edge {
         case Top
@@ -184,57 +173,43 @@ extension UIView {
     ///
     /// Specifies how a view will be aligned relative to the sibling view.
     ///
-    /// ----
     /// **ToTheRightMatchingTop**: Specifies that the view should be aligned to the right of a sibling, matching the
     /// top, or y origin, of the sibling's frame.
     ///
-    /// ----
     /// **ToTheRightMatchingBottom**: Specifies that the view should be aligned to the right of a sibling, matching
     /// the bottom, or max y value, of the sibling's frame.
     ///
-    /// ----
     /// **ToTheRightCentered**: Specifies that the view should be aligned to the right of a sibling, and will be centered
     /// to either match the vertical center of the sibling's frame or centered vertically within the superview, depending
     /// on the context.
     ///
-    /// ----
     /// **ToTheLeftMatchingTop**: Specifies that the view should be aligned to the left of a sibling, matching the top,
     /// or y origin, of the sibling's frame.
     ///
-    /// ----
     /// **ToTheLeftMatchingBottom**: Specifies that the view should be aligned to the left of a sibling, matching the
     /// bottom, or max y value, of the sibling's frame.
     ///
-    /// ----
     /// **ToTheLeftCentered**: Specifies that the view should be aligned to the left of a sibling, and will be centered
     /// to either match the vertical center of the sibling's frame or centered vertically within the superview, depending
     /// on the context.
     ///
-    /// ---
     /// **UnderMatchingLeft**: Specifies that the view should be aligned under a sibling, matching the left, or x origin,
     /// of the sibling's frame.
     ///
-    /// ----
     /// **UnderMatchingRight**: Specifies that the view should be aligned under a sibling, matching the right, or max y
     /// of the sibling's frame.
     ///
-    /// ----
     /// **UnderCentered**: Specifies that the view should be aligned under a sibling, and will be centered to either match
     /// the horizontal center of the sibling's frame or centered horizontally within the superview, depending on the context.
     ///
-    /// ----
     /// **AboveMatchingLeft**: Specifies that the view should be aligned above a sibling, matching the left, or x origin
     /// of the sibling's frame.
     ///
-    /// ----
     /// **AboveMatchingRight**: Specifies that the view should be aligned above a sibling, matching the right, or max x
     /// of the sibling's frame.
     ///
-    /// ----
     /// **AboveCentered**: Specifies that the view should be aligned above a sibling, and will be centered to either match
     /// the horizontal center of the sibling's frame or centered horizontally within the superview, depending on the context.
-    ///
-    /// ----
     ///
     enum Align {
         case ToTheRightMatchingTop
@@ -257,13 +232,9 @@ extension UIView {
     ///
     /// Specifies how a group will be laid out.
     ///
-    /// ----
-    /// **Horizontal**: Specifies that the views should be aligned relative to to eachother horizontally.
+    /// **Horizontal**: Specifies that the views should be aligned relative to eachother horizontally.
     ///
-    /// ----
     /// **Vertical**: Specifies that the views should be aligned relative to eachother vertically.
-    ///
-    /// ----
     ///
     enum Group {
         case Horizontal
