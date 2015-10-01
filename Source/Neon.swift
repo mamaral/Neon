89331bb0e14760436ef22e0ff556256e18fd59dd
Mike Amaral <mike.amaral@ping4.com>
2015-10-01 19:44:55 -0400

    Commit before I get too far into this.

--- Source/Neon.swift
+++ Source/Neon.swift
@@ -8,122 +8,22 @@
 
 import UIKit
 
+extension UIView : Frameable, Anchorable, Groupable {}
+extension CALayer : Frameable, Anchorable, Groupable {}
+
+protocol Alignable {
+
+}
+
+protocol Groupable {
+
+}
+
 extension UIView {
 
 
     // MARK: Frame shortcuts
     //
-
-    /// Get the x origin of a view.
-    ///
-    /// - returns: The minimum x value of the view's frame.
-    ///
-    /// Example
-    /// -------
-    ///     let frame = CGRectMake(10.0, 20.0, 5.0, 7.0)
-    ///     frame.x() // returns 10.0
-    ///
-    func x() -> CGFloat {
-        return CGRectGetMinX(frame)
-    }
-
-
-    /// Get the mid x of a view.
-    ///
-    /// - returns: The middle x value of the view's frame
-    ///
-    /// Example
-    /// -------
-    ///     let frame = CGRectMake(10.0, 20.0, 5.0, 7.0)
-    ///     frame.midX() // returns 7.5
-    ///
-    func xMid() -> CGFloat {
-        return CGRectGetMinX(frame) + (CGRectGetWidth(frame) / 2.0)
-    }
-
-
-    /// Get the max x of a view.
-    ///
-    /// - returns: The maximum x value of the view's frame
-    ///
-    /// Example
-    /// -------
-    ///     let frame = CGRectMake(10.0, 20.0, 5.0, 7.0)
-    ///     frame.maxX() // returns 15.0
-    ///
-    func xMax() -> CGFloat {
-        return CGRectGetMaxX(frame)
-    }
-
-
-    /// Get the y origin of a view.
-    ///
-    /// - returns: The minimum y value of the view's frame.
-    ///
-    /// Example
-    /// -------
-    ///     let frame = CGRectMake(10.0, 20.0, 5.0, 7.0)
-    ///     frame.y() // returns 20.0
-    ///
-    func y() -> CGFloat {
-        return CGRectGetMinY(frame)
-    }
-
-
-    /// Get the mid y of a view.
-    ///
-    /// - returns: The middle y value of the view's frame
-    ///
-    /// Example
-    /// -------
-    ///     let frame = CGRectMake(10.0, 20.0, 5.0, 7.0)
-    ///     frame.midY() // returns 13.5
-    ///
-    func yMid() -> CGFloat {
-        return CGRectGetMinY(frame) + (CGRectGetHeight(frame) / 2.0)
-    }
-
-
-    /// Get the max y of a view.
-    ///
-    /// - returns: The maximum y value of the view's frame.
-    ///
-    /// Example
-    /// -------
-    ///     let frame = CGRectMake(10.0, 20.0, 5.0, 7.0)
-    ///     frame.maxY() // returns 27.0
-    ///
-    func yMax() -> CGFloat {
-        return CGRectGetMaxY(frame)
-    }
-
-
-    /// Get the width of a view.
-    ///
-    /// - returns: The width of the view's frame.
-    ///
-    /// Example
-    /// -------
-    ///     let frame = CGRectMake(10.0, 20.0, 5.0, 7.0)
-    ///     frame.width() // returns 5.0
-    ///
-    func width() -> CGFloat {
-        return CGRectGetWidth(frame)
-    }
-
-
-    /// Get the height of a view.
-    ///
-    /// - returns: The height of the view's frame.
-    ///
-    /// Example
-    /// -------
-    ///     let frame = CGRectMake(10.0, 20.0, 5.0, 7.0)
-    ///     frame.height() // returns 7.0
-    ///
-    func height() -> CGFloat {
-        return CGRectGetHeight(frame)
-    }
 
 
     // MARK: Corner
---
+++ Source/NeonAnchorable.swift
@@ -0,0 +1,17 @@
+//
+//  NeonAnchorable.swift
+//  Neon
+//
+//  Created by Mike on 10/1/15.
+//  Copyright © 2015 Mike Amaral. All rights reserved.
+//
+
+import UIKit
+
+protocol Anchorable {
+
+}
+
+extension Anchorable {
+    
+}
