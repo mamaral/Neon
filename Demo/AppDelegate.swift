82b7679cc752bc38388a5e1e0dcb31b726ac7f3f
Mike Amaral <mike.amaral@ping4.com>
2015-10-03 08:29:15 -0400

    Changed default demo back to the Facebook profile example.

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -19,16 +19,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // The facebook example from the README.... Don't ask why the file name is FacebookProfileExampleViewController
         // but the class referenced here is TwitterProfileExampleViewController... I didn't realize you can't yet refactor
         // swift code, and somehow this still works...
-//        UINavigationBar.appearance().barTintColor = UIColor(red: 80/255.0, green: 108/255.0, blue: 163/255.0, alpha: 1.0)
-//        UINavigationBar.appearance().translucent = false
-//        self.window?.rootViewController = UINavigationController(rootViewController: TwitterProfileExampleViewController())
+        UINavigationBar.appearance().barTintColor = UIColor(red: 80/255.0, green: 108/255.0, blue: 163/255.0, alpha: 1.0)
+        UINavigationBar.appearance().translucent = false
+        self.window?.rootViewController = UINavigationController(rootViewController: TwitterProfileExampleViewController())
 
         // Used for tinkering and testing new methods - uncomment this out if you want to experiment!
 //        self.window?.rootViewController = TestViewController()
 
         // This is crazy-complex demo I have on the README. Uncomment this out to experience with that as well,
         // although I really built it for iPads.
-        self.window?.rootViewController = ViewController()
+//        self.window?.rootViewController = ViewController()
 
         self.window?.makeKeyAndVisible()
         return true
